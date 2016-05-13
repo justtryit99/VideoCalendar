54b37c2aa9c974f889c6b37c28dcbdf0b4ec46fe
JUMP <Jump@Jump-MacBook-Air.local>
2016-05-13 17:29:22 +0800

    1.修改UI

---
+++ objectCase/ImageBrowser.h
@@ -0,0 +1,13 @@
+//
+//  ImageBrowser.h
+//  objectCase
+//
+//  Created by JUMP on 2016/5/13.
+//  Copyright © 2016年 Andy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ImageBrowser : NSObject
+
+@end
---
+++ objectCase/ImageBrowser.m
@@ -0,0 +1,13 @@
+//
+//  ImageBrowser.m
+//  objectCase
+//
+//  Created by JUMP on 2016/5/13.
+//  Copyright © 2016年 Andy. All rights reserved.
+//
+
+#import "ImageBrowser.h"
+
+@implementation ImageBrowser
+
+@end
